3c3880e1580a3ed16af5067463a3e7ec05dc96d2
Illés Gergő <illes.gergo1@gmail.com>
2024-01-12 07:40:45 +0100

    Linebreak, scrolloff

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set expandtab")
+vim.cmd("set linebreak")
+vim.cmd("set scrolloff=10")
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.cmd("tnoremap <Esc> <C-\\><C-N>")
