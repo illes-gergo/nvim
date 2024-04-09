15944053dc69dc23981683d39b57dcd652678cc7
Illés Gergő <illes.gergo1@gmail.com>
2024-04-09 14:28:40 +0200

    Zathura inverse search

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,8 +1,9 @@
 return {
- "lervag/vimtex",
-  ft = {'tex','plaintex'},
-  config = function ()
+  "lervag/vimtex",
+  ft = { 'tex', 'plaintex' },
+  config = function()
     vim.g.vimtex_view_method = 'zathura'
-    vim.g.vimtex_compiler_merthod = 'latexrun'
+    vim.g.vimtex_compiler_progname = 'nvr'
+    vim.g.tex_flavour = 'latex'
   end
 }
