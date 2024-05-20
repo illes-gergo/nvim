1997bc13e0a53b81a5737ebf74db83c5f1439e6a
Illés Gergő <illes.gergo1@gmail.com>
2024-05-20 15:04:26 +0200

    search for hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,6 +22,11 @@ return {
 						}),
 					},
 				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
 			})
 			-- To get ui-select loaded and working with telescope, you need to call
 			-- load_extension, somewhere after setup function:
