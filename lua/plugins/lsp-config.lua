a8acc54da86aee3ee0902beb2383b33238455f06
Illés Gergő <illes.gergo1@gmail.com>
2024-10-26 15:20:46 +0200

    removed conflict

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,16 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "julials", "clangd", "fortls" , "bashls" },
-
---     bash-language-server bashls
---     clang-format
---     clangd
---     cpplint
---     findent
---     fortls
---     julia-lsp julials
---     lua-language-server lua_ls
+--        ensure_installed = { "lua_ls", "julials", "clangd" },
       })
     end,
   },
@@ -30,33 +21,33 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       capabilities.offsetEncoding = { "utf-16" }
       local lspconfig = require("lspconfig")
-      lspconfig.julials.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.fortls.setup({
-        capabilities = capabilities,
-        cmd = {
-          'fortls',
-          '--lowercase_intrinsics',
-          '--hover_signature',
-          '--hover_language=fortran',
-          '--use_signature_help',
-          '--autocomplete_no_snippets',
-          '--autocomplete_name_only',
-          '--enable_code_actions',
-        }
-      })
-      lspconfig.clangd.setup({
-        capabilities = {
-          offsetEncoding = "utf-16",
-        },
-        init_options = {
-          usePlaceholders = false,
-        },
-      })
+ --     lspconfig.lua_ls.setup({
+ --       capabilities = capabilities,
+ --     })
+ --     lspconfig.julials.setup({
+ --       capabilities = capabilities,
+ --     })
+ --     lspconfig.fortls.setup({
+ --       capabilities = capabilities,
+ --       cmd = {
+ --         'fortls',
+ --         '--lowercase_intrinsics',
+ --         '--hover_signature',
+ --         '--hover_language=fortran',
+ --         '--use_signature_help',
+ --         '--autocomplete_no_snippets',
+ --         '--autocomplete_name_only',
+ --         '--enable_code_actions',
+ --       }
+ --     })
+ --     lspconfig.clangd.setup({
+ --       capabilities = {
+ --         offsetEncoding = "utf-16",
+ --       },
+ --       init_options = {
+ --         usePlaceholders = false,
+ --       },
+ --     })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
