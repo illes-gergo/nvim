4b92da558399fbcfd27812677b20cb3f406f4206
Illés Gergő <illes.gergo1@gmail.com>
2025-02-15 03:10:54 +0100

    resolved conflict

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,12 +5,12 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format,
-				require("none-ls.diagnostics.cpplint"),
-				require("none-ls.formatting.latexindent"),
-        null_ls.builtins.formatting.findent,
-				--null_ls.builtins.diagnostics.vale,
+---				null_ls.builtins.formatting.stylua,
+---				null_ls.builtins.formatting.clang_format,
+---				require("none-ls.diagnostics.cpplint"),
+---				require("none-ls.formatting.latexindent"),
+---       null_ls.builtins.formatting.findent,
+---	     	null_ls.builtins.diagnostics.vale,
 			},
 			debug = true,
 		})
