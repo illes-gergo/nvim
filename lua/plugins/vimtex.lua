d71340af64b19bd600db023a0a14784382f7360d
Illés Gergő <illes.gergo1@gmail.com>
2025-04-01 12:03:20 +0200

    xelatex as default

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,8 +2,8 @@ return {
   "lervag/vimtex",
   ft = { 'tex', 'plaintex' },
   config = function()
-    vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_view_method = 'zathura_simple'
     vim.g.vimtex_compiler_progname = 'nvr'
-    vim.g.tex_flavour = 'latex'
+    vim.g.tex_flavour = 'xelatex'
   end
 }
