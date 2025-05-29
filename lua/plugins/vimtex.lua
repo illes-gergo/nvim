e3f04810a9f19ba6761a157fef1a0c52ef6b6bfa
Illés Gergő <illes.gergo1@gmail.com>
2025-05-29 07:48:32 +0200

    removed virtual stuff

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,6 @@ return {
   ft = { 'tex', 'plaintex' },
   config = function()
     vim.g.vimtex_view_method = 'zathura_simple'
-    vim.g.vimtex_compiler_progname = 'nvr'
-    vim.g.tex_flavour = 'xelatex'
+    vim.g.vimtex_tex_flavour = 'xelatex'
   end
 }
