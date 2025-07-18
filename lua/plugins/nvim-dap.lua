e912c337997e493fd6929fb3a5230a777a081126
Illés Gergő <illes.gergo1@gmail.com>
2025-07-18 15:36:34 +0200

    configuring dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -7,22 +7,32 @@ return {
   config = function()
     local dap, dapui = require("dap"), require("dapui")
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+    --dap.listeners.before.attach.dapui_config = function()
+    --  dapui.open()
+    --end
+    --dap.listeners.before.launch.dapui_config = function()
+    --  dapui.open()
+    --end
+    --dap.listeners.before.event_terminated.dapui_config = function()
+    --  dapui.close()
+    --end
+    --dap.listeners.before.event_exited.dapui_config = function()
+    --  dapui.close()
+    --end
 
-    vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint, {})
-    vim.keymap.set('n', '<Leader>dB', dap.set_breakpoint, {})
-    vim.keymap.set('n', '<Leader>dc', dap.continue, {})
+    --vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint, {})
+    --vim.keymap.set('n', '<Leader>dB', dap.set_breakpoint, {})
+    vim.keymap.set('n', '<Leader>dc', dapui.toggle, {})
+    --
+    vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+    vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+    vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+    vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+    vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+    vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+    vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+    vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+    vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
 
     dap.adapters.gdb = {
       type = "executable",
@@ -37,6 +47,7 @@ return {
         program = function()
           return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
+        args = function() return vim.split(vim.fn.input('Arguments: '), " +") end,
         cwd = "${workspaceFolder}",
         stopAtBeginningOfMainSubprogram = false,
       },
@@ -58,10 +69,6 @@ return {
         type = 'gdb',
         request = 'attach',
         target = 'localhost:1234',
-        program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}'
       },
     }
     dap.configurations.cpp=dap.configurations.c
